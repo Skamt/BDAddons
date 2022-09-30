07e8cf93e573dc48da6c6944eb9f62fc6f0fb735
Aaaaa <114232893+Skamt@users.noreply.github.com>
2022-09-30 14:07:49 +0100

    stuff

--- src/plugins/FreeStickers/index.js
+++ src/plugins/FreeStickers/index.js
@@ -14,6 +14,7 @@ module.exports = (Plugin, Api) => {
 		}
 	} = Api;
 
+	const showToast = (c, o) => { BdApi.showToast(`${config.info.name}: ${c}`, o) };
 	const ANIMATED_STICKER_TAG = "ANIMATED_STICKER_TAG";
 	const LOTTIE_STICKER_TAG = "LOTTIE_STICKER_TAG";
 	const getStickerSendability = WebpackModules.getModule(Filters.byString("SENDABLE_WITH_PREMIUM", "canUseStickersEverywhere"));
@@ -21,13 +22,10 @@ module.exports = (Plugin, Api) => {
 	const StickSendEnum = WebpackModules.getByProps("SENDABLE_WITH_BOOSTED_GUILD");
 	const StickerFormat = WebpackModules.getByProps("APNG", "LOTTIE");
 	const ComponentDispatch = WebpackModules.getModule(m => m.dispatchToLastSubscribed && m.emitter.listeners("INSERT_TEXT").length);
-
-	const listeners = [];
-	const removeListeners = () => listeners.forEach(({ el, event, func }) => el.removeEventListener(event, func));
-
-	const addListener = (el, event, func) => {
-		el.addEventListener(event, func);
-		listeners.push({ el, event, func });
+	const STRINGS = {
+		sendLottieSticker: "Official Discord Stickers are not supported.",
+		missingEmbedPermissions: "Missing Embed Permissions",
+		disabledAnimatedStickers: "You have disabled animated stickers in settings."
 	};
 
 	const css = require("styles.css");
@@ -35,17 +33,21 @@ module.exports = (Plugin, Api) => {
 	return class FreeStickers extends Plugin {
 		constructor() {
 			super();
+			this.stickerClickHandler = this.stickerClickHandler.bind(this);
 		}
 
 		onStop() {
+			document.removeEventListener("click", this.stickerClickHandler);
 			PluginUtilities.removeStyle(this.getName());
 			Patcher.unpatchAll();
-			removeListeners();
 		}
 
 		onStart() {
 			try {
-				this.patch();
+				document.addEventListener("click", this.stickerClickHandler);
+				PluginUtilities.addStyle(this.getName(), css);
+				this.patchGetStickerById();
+				this.updateStickers();
 			} catch (e) {
 				console.error(e);
 			}
@@ -54,28 +56,25 @@ module.exports = (Plugin, Api) => {
 		getSettingsPanel() {
 			const panel = this.buildSettingsPanel();
 			panel.addListener((id, checked) => {
-				if (id === "highlightanimated")
+				if (id === "highlightAnimated")
 					this.updateStickers();
 			});
 			return panel.getElement();
 		}
 
-		patch() {
-			this.patchGetStickerById();
-			PluginUtilities.addStyle(this.getName(), css);
-			addListener(document, "click", (e) => { this.stickerClickHandler(e) });
-			this.updateStickers();
-		}
-
 		updateStickers() {
-			// trigger the patch
 			StickerStore.stickerMetadata.forEach((value, key) => StickerStore.getStickerById(key));
 		}
 
 		checkEmbedPerms(channelId) {
 			const channel = ChannelStore.getChannel(channelId);
 			if (!channel.guild_id) return true;
-			return Permissions.can({ permission: 16384n, context: channel, user: UserStore.getCurrentUser().id });
+			const embedPerms = Permissions.can({ permission: 16384n, context: channel, user: UserStore.getCurrentUser().id });
+			return embedPerms || this.settings.ignoreEmbedPermissions;
+		}
+
+		sendAniamted(sticker) {
+			return sticker["format_type"] === StickerFormat.APNG && !this.settings.sendAnimatedStickers
 		}
 
 		isLottieSticker(sticker) {
@@ -89,41 +88,55 @@ module.exports = (Plugin, Api) => {
 			return [StickerSendableState, sticker];
 		}
 
+		checkSendAsLink(sticker, channelId) {
+			if (this.isLottieSticker(sticker))
+				return showToast(STRINGS.sendLottieSticker, { type: 'danger' });
+			if (this.sendAniamted(sticker))
+				return showToast(STRINGS.disabledAnimatedStickers, { type: 'info' })
+			if (!this.checkEmbedPerms(channelId))
+				return showToast(STRINGS.missingEmbedPermissions, { type: 'info' })
+
+			this.sendStickerAsLink(sticker, channelId);
+		}
+
 		sendStickerAsLink(sticker, channelId) {
-			if (this.isLottieSticker(sticker)) return;
-			if (sticker["format_type"] === StickerFormat.APNG && !this.settings.sendAnimatedStickers) return;
 			const url = `https://media.discordapp.net/stickers/${sticker.id}.webp?size=${this.settings.stickerSize}&passthrough=false&quality=lossless`;
-			if (this.checkEmbedPerms(channelId))
-				if (this.settings.sendDirectly)
-			MessageActions.sendMessage(channelId, {
-						content: url,
-						validNonShortcutEmojis: []
-					});
-				else
-					ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
-						plainText: url
-					})
+			if (this.settings.sendDirectly)
+				MessageActions.sendMessage(channelId, {
+					content: url,
+					validNonShortcutEmojis: []
+				});
 			else
-				BdApi.showToast("Missing Embed Permissions");
+				ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
+					plainText: url
+				})
+		}
+
+		stickerHandler({ target, props }) {
+			const channel = ChannelStore.getChannel(SelectedChannelStore.getChannelId());
+			const stickerId = props["data-id"];
+			const [sendable, sticker] = this.isStickerSendable(stickerId, channel);
+			if (!sendable)
+				this.checkSendAsLink(sticker, channel.id);
 		}
 
 		stickerClickHandler(e) {
-			const targetProps = e.target.__reactProps$;
-			if (targetProps && targetProps["data-type"] && targetProps["data-type"].toLowerCase() === "sticker") {
-				console.log(e.target);
-				console.log(targetProps);
-				const channel = ChannelStore.getChannel(SelectedChannelStore.getChannelId());
-				const stickerId = targetProps["data-id"];
-				const [sendable, sticker] = this.isStickerSendable(stickerId, channel);
-				console.log(sendable, sticker)
-				if (!sendable) {
-					this.sendStickerAsLink(sticker, channel.id);
-				}
+			const props = e.target.__reactProps$;
+			if (props && props["data-type"] && props["data-type"].toLowerCase() === "sticker") {
+				this.stickerHandler({ target: e.target, props });
 			}
 		}
 
-		patcheStickerStore(args, ret) {
-			const sticker = ret;
+		patchGetStickerById() {
+			Patcher.after(StickerStore, "getStickerById", (_, args, ret) => {
+				if (this.settings.highlightAnimated)
+					this.patcheStickerStore(ret);
+				else
+					this.cleanStickerStore(ret);
+			});
+		}
+
+		patcheStickerStore(sticker) {
 			if (sticker && !sticker.fixed) {
 				let result = "";
 				if (this.isLottieSticker(sticker))
@@ -134,21 +147,12 @@ module.exports = (Plugin, Api) => {
 				sticker.fixed = true;
 			}
 		}
-		unpatcheStickerStore(args, ret) {
-			const sticker = ret;
+		cleanStickerStore(sticker) {
 			if (sticker && sticker.fixed) {
 				sticker.description = sticker.description.replace(LOTTIE_STICKER_TAG, '').replace(ANIMATED_STICKER_TAG, '');
 				sticker.fixed = false;
 			}
 		}
 
-		patchGetStickerById() {
-			Patcher.after(StickerStore, "getStickerById", (_, args, ret) => {
-				if (this.settings.highlightanimated)
-					this.patcheStickerStore(args, ret);
-				else
-					this.unpatcheStickerStore(args, ret);
-			});
-		}
 	};
 };
