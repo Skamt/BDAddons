0f4a8f1252bfa8994a99e4b33c2fe5cabf681982
Aaaaa <114232893+Skamt@users.noreply.github.com>
2022-10-16 22:47:30 +0100

    StickerEmojiPreview

---
+++ src/plugins/StickerEmojiPreview/index.js
@@ -0,0 +1,47 @@
+module.exports = (API) => {
+	const { 
+		React,
+		Patcher, 
+		DOM,
+		Webpack: { Filters, getModule }
+	} = API;
+	const Popout = getModule(Filters.byStrings("renderPopout", "animationPosition"), { searchExports: true });
+	const ExpressionPickerInspector = getModule((m) => m.Z && m.Z.toString().includes("EMOJI_IS_FAVORITE_ARIA_LABEL"));
+	
+	// components
+	const previewComponent = require("components/previewComponent.jsx");
+	// styles
+	const css = require("styles.css");
+
+	return class StickerEmojiPreview  {
+
+		start() {
+			try {
+				DOM.addStyle(config.info.name,css);
+				Patcher.after(config.info.name,ExpressionPickerInspector, "Z", (_, args, ret) => {
+					if (ret.props.children[0].props.children.props.sticker)
+						return React.createElement(previewComponent, {
+							previewSize: 300,
+							sticker: true,
+							element: ret,
+							data: ret.props.children[0].props.children.props.sticker
+						})
+					else {
+						return React.createElement(previewComponent, {
+							previewSize: 300,
+							element: ret,
+							data: ret.props.children[0].props.children.props.src || ""
+						})
+					}
+				})
+			} catch (e) {
+				console.error(e);
+			}
+		}
+
+		stop() {
+			DOM.removeStyle(config.info.name);
+			Patcher.unpatchAll(config.info.name);
+		}
+	};
+}
