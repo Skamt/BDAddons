9d231cf546b83847d20b741b487801cbc06f79a5
Aaaaa <114232893+Skamt@users.noreply.github.com>
2022-10-17 16:23:43 +0100

    added keybind to toggle the preview

--- src/plugins/StickerEmojiPreview/components/previewComponent.jsx
+++ src/plugins/StickerEmojiPreview/components/previewComponent.jsx
@@ -1,5 +1,14 @@
 module.exports = ({ sticker, element, data, previewSize }) => {
 	const url = sticker ? `https://media.discordapp.net/stickers/${data.id}.webp?size=640&quality=lossless` : data.replace(/(size=)(\d+)[&]/, `&size=640`);
+	const [show, setShow] = useState(false);
+
+	useEffect(() => {
+		function keyupHandler() {
+			setShow(!show);
+		}
+		document.addEventListener("keyup", keyupHandler);
+		return () => document.removeEventListener("keyup", keyupHandler);
+	}, [show]);
 	return (
 		<Popout
 			renderPopout={() => (
@@ -9,7 +18,7 @@ module.exports = ({ sticker, element, data, previewSize }) => {
 					<img src={url}></img>
 				</div>
 			)}
-			shouldShow={true}
+			shouldShow={show}
 			position={Popout.Positions.LEFT}
 			align={Popout.Align.BOTTOM}
 			animation={Popout.Animation["SCALE"]}
--- src/plugins/StickerEmojiPreview/index.js
+++ src/plugins/StickerEmojiPreview/index.js
@@ -1,24 +1,26 @@
 module.exports = (API) => {
-	const { 
-		React,
-		Patcher, 
+	const {
 		DOM,
+		React,
+		Patcher,
+		React: { useEffect, useState },
 		Webpack: { Filters, getModule }
 	} = API;
+
+	// Modules
 	const Popout = getModule(Filters.byStrings("renderPopout", "animationPosition"), { searchExports: true });
 	const ExpressionPickerInspector = getModule((m) => m.Z && m.Z.toString().includes("EMOJI_IS_FAVORITE_ARIA_LABEL"));
-	
+
 	// components
 	const previewComponent = require("components/previewComponent.jsx");
 	// styles
 	const css = require("styles.css");
 
-	return class StickerEmojiPreview  {
-
+	return class StickerEmojiPreview {
 		start() {
 			try {
-				DOM.addStyle(config.info.name,css);
-				Patcher.after(config.info.name,ExpressionPickerInspector, "Z", (_, args, ret) => {
+				DOM.addStyle(config.info.name, css);
+				Patcher.after(config.info.name, ExpressionPickerInspector, "Z", (_, args, ret) => {
 					if (ret.props.children[0].props.children.props.sticker)
 						return React.createElement(previewComponent, {
 							previewSize: 300,
