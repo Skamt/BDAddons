f7e01aa4d3953fe8b4ca66d5926d324078daf976
Aaaaa <114232893+Skamt@users.noreply.github.com>
2022-10-18 15:31:37 +0100

    StickerEmojiPreview

---
+++ StickerEmojiPreview/README.md
@@ -0,0 +1,14 @@
+# StickerEmojiPreview
+
+Adds a zoomed preview to those tiny Stickers and Emojis
+
+## Usage:
+
+After opening the Emojis/Sticker picker press any key to open/close the preview
+
+## Preview
+
+![preview1](https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/assets/preview1.png)
+![preview2](https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/assets/preview2.png)
+![preview3](https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/assets/preview3.png)
+![preview4](https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/assets/preview4.png)
---
+++ StickerEmojiPreview/StickerEmojiPreview.plugin.js
@@ -0,0 +1,103 @@
+/**
+ * @name StickerEmojiPreview
+ * @description Adds a zoomed preview to those tiny Stickers and Emojis
+ * @version 1.0.0
+ * @author Skamt
+ * @website https://github.com/Skamt/BDAddons/tree/main/StickerEmojiPreview
+ * @source https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/StickerEmojiPreview.plugin.js
+ */
+const config = {
+	info: {
+		name: "StickerEmojiPreview",
+		version: "1.0.0",
+		description: "Adds a zoomed preview to those tiny Stickers and Emojis",
+		source: "https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/StickerEmojiPreview.plugin.js",
+		github: "https://github.com/Skamt/BDAddons/tree/main/StickerEmojiPreview",
+		authors: [{
+			name: "Skamt"
+		}]
+	}
+};
+module.exports = ((API) => {
+	const {
+		DOM,
+		React,
+		Patcher,
+		React: { useEffect, useState },
+		Webpack: { Filters, getModule }
+	} = API;
+	// Modules
+	const Popout = getModule(Filters.byStrings("renderPopout", "animationPosition"), { searchExports: true });
+	const ExpressionPickerInspector = getModule((m) => m.Z && m.Z.toString().includes("EMOJI_IS_FAVORITE_ARIA_LABEL"));
+	// Constants
+	const PREVIEW_SIZE = 300;
+	// components
+	const previewComponent = ({ target, previewComponent, previewSize }) => {
+		const [show, setShow] = useState(false);
+		useEffect(() => {
+			function keyupHandler() {
+				setShow(!show);
+			}
+			document.addEventListener("keyup", keyupHandler);
+			return () => document.removeEventListener("keyup", keyupHandler);
+		}, [show]);
+		return (
+			React.createElement(Popout, {
+					renderPopout: () =>
+						React.createElement("div", {
+								className: "stickersPreview",
+								style: { width: `${previewSize}px` }
+							},
+							previewComponent),
+					shouldShow: show,
+					position: Popout.Positions.LEFT,
+					align: Popout.Align.BOTTOM,
+					animation: Popout.Animation["SCALE"],
+					spacing: 60
+				},
+				() => target));
+	};;
+	// styles
+	const css = `.stickersPreview {
+	width:400px;
+	font-size: 14px;
+	background: var(--background-floating);
+	border-radius: 5px;
+	padding: .5em;
+	box-shadow: var(--elevation-high);
+}
+.stickersPreview img{
+	min-width:100%;
+	max-width:100%;
+}
+.animated img{
+	border:1px dashed #ff8f09;
+	padding:1px;
+	box-sizing:border-box;
+}`;
+	return class StickerEmojiPreview {
+		start() {
+			try {
+				DOM.addStyle(config.info.name, css);
+				Patcher.after(config.info.name, ExpressionPickerInspector, "Z", (_, [{ graphicPrimary }], ret) => {
+					return React.createElement(previewComponent, {
+						previewSize: PREVIEW_SIZE,
+						target: ret,
+						previewComponent: React.createElement(graphicPrimary.type, {
+							...graphicPrimary.props,
+							src: graphicPrimary.props.src?.replace(/([?&]size=)(\d+)/, `$1${PREVIEW_SIZE}`),
+							disableAnimation: false,
+							size: PREVIEW_SIZE
+						})
+					})
+				})
+			} catch (e) {
+				console.error(e);
+			}
+		}
+		stop() {
+			DOM.removeStyle(config.info.name);
+			Patcher.unpatchAll(config.info.name);
+		}
+	};
+})(BdApi)
