fbce0b54512c6984622fc5994b3dd022f3a18fea
Aaaaa <114232893+Skamt@users.noreply.github.com>
2022-10-19 00:56:02 +0100

    switch to BdApi

--- src/plugins/CopyImageLink/components/copyButton.jsx
+++ src/plugins/CopyImageLink/components/copyButton.jsx
@@ -1,12 +1,12 @@
-module.exports = ({ onClick }) => {
+module.exports = ({ href }) => {
 	return (
 		<>
 			<span className="copyBtnSpan">|</span>
 			<a
 				className="downloadLink-1OAglv anchorUnderlineOnHover-2qPutX copyBtn"
-				onClick={onClick}>
+				onClick={_ => Utils.copy(href)}>
 				Copy link
 			</a>
 		</>
 	);
-};
+}
