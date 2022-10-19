9fdbcb2c3b83cc988579dd85cb877d870a093b75
Aaaaa <114232893+Skamt@users.noreply.github.com>
2022-10-19 01:43:17 +0100

    prep for no Zlib

--- scripts/builder/buildPlugin.js
+++ scripts/builder/buildPlugin.js
@@ -23,7 +23,7 @@ module.exports = (pluginRootFile, pluginFolder, pluginFiles, config) => {
 	const content = parser(pluginRootFile, pluginFolder, pluginFiles);
 	let result = buildMeta(config);
 	if (process.argv.slice(2).length) {
-		result += `{{CONFIG_TEMPLATE}}module.exports = ({{PLUGIN_TEMPLATE}})(BdApi)`;
+		result += `{{CONFIG_TEMPLATE}}module.exports = ({{PLUGIN_TEMPLATE}})()`;
 		result = result.replace(`{{CONFIG_TEMPLATE}}`, `const config = ${beautify(JSON.stringify(config),{"brace_style": "collapse"}).replace(/"((?:[A-Za-z]|[0-9]|_)+)"\s?:/g, "$1:")};`)
 		result = result.replace(`{{PLUGIN_TEMPLATE}}`, `${content}`);
 	} else {
