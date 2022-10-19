4990542322c87389d61b40563e111da32b47d59f
Aaaaa <114232893+Skamt@users.noreply.github.com>
2022-10-19 01:39:39 +0100

    prep for no Zlib

--- src/plugins/ViewProfilePicture/index.js
+++ src/plugins/ViewProfilePicture/index.js
@@ -1,20 +1,19 @@
-module.exports = (Plugin, Api) => {
-	const { Filters, getModule } = BdApi.Webpack;
+module.exports = () => {
 	const {
-		Logger,
+		UI,
+		DOM,
+		React,
 		Patcher,
-		Toasts,
-		Utilities,
-		PluginUtilities,
-		DiscordModules: {
-			React,
-			ModalActions,
-			SelectedGuildStore
+		Webpack: {
+			Filters,
+			getModule
 		}
-	} = Api;
+	} = BdApi;
 
 	// Modules
-	const CurrentUserStore = getModule(Filters.byProps("getCurrentUser", "getUsers"));;
+	const openModal = getModule(Filters.byStrings("onCloseCallback", "Layer"), { searchExports: true });
+	const SelectedGuildStore = getModule(Filters.byProps("getLastSelectedGuildId"));
+	const CurrentUserStore = getModule(Filters.byProps("getCurrentUser", "getUsers"));
 	const UserBannerMask = getModule((m) => m.Z && m.Z.toString().includes("overrideAvatarDecorationURL"));
 	const ProfileTypeEnum = getModule(Filters.byProps("POPOUT"), { searchExports: true });
 	const ImageModal = getModule(m => m?.prototype?.render?.toString().includes("OPEN_ORIGINAL_IMAGE"));
@@ -28,12 +27,15 @@ module.exports = (Plugin, Api) => {
 
 	// Helper functions
 	const Utils = {
-		showToast: (content, type) => Toasts[type](`[${config.info.name}] ${content}`),
+		showToast: (content, type) => UI.showToast(`[${config.info.name}] ${content}`, { type }),
 		copy: (data) => {
 			DiscordNative.clipboard.copy(data);
-			Utils.showToast(data, "info");
-			Utils.showToast("Copied!", "success");
+			Utils.showToast("Color Copied!", "success");
 		},
+		/* Stolen from Zlib until it gets added to BdApi */
+		getNestedProp: (obj, path) => path.split(".").reduce(function(ob, prop) {
+			return ob && ob[prop];
+		}, obj),
 		getImageModalComponent: (Url, props) => React.createElement(ImageModal, {
 			...props,
 			src: Url,
@@ -56,7 +58,7 @@ module.exports = (Plugin, Api) => {
 		}
 
 		openCarousel(items) {
-			ModalActions.openModal(props => React.createElement(DisplayCarousel, { props, items }));
+			openModal(props => React.createElement(DisplayCarousel, { props, items }));
 		}
 
 		clickHandler(userObject, bannerStyleObject, isUserPopout) {
@@ -71,19 +73,19 @@ module.exports = (Plugin, Api) => {
 		}
 
 		patchUserBannerMask() {
-			Patcher.after(UserBannerMask, "Z", (_, [{ user, isPremium, profileType }], returnValue) => {
+			Patcher.after(this.name, UserBannerMask, "Z", (_, [{ user, isPremium, profileType }], returnValue) => {
 				const currentUser = CurrentUserStore.getCurrentUser();
 				let bannerStyleObject, children, className = "VPP-Button";
 				if (ProfileTypeEnum.MODAL === profileType || ProfileTypeEnum.POPOUT === profileType) {
-					bannerStyleObject = Utilities.getNestedProp(returnValue, "props.children.1.props.children.props.style");
-					children = Utilities.getNestedProp(returnValue, "props.children.1.props.children.props.children");
+					bannerStyleObject = Utils.getNestedProp(returnValue, "props.children.1.props.children.props.style");
+					children = Utils.getNestedProp(returnValue, "props.children.1.props.children.props.children");
 					if (user.id === currentUser.id) className += " VPP-current"
 					if (isPremium) className += " VPP-premium"
 					if (!isPremium && user.id !== currentUser.id) className += " VPP-normal"
 					if (ProfileTypeEnum.MODAL === profileType) className += " VPP-profile"
 				} else if (ProfileTypeEnum.SETTINGS === profileType) {
-					bannerStyleObject = Utilities.getNestedProp(returnValue, "props.children.props.style");
-					children = Utilities.getNestedProp(returnValue, "props.children.props.children");
+					bannerStyleObject = Utils.getNestedProp(returnValue, "props.children.props.style");
+					children = Utils.getNestedProp(returnValue, "props.children.props.children");
 					className += " VPP-settings VPP-normal"
 				}
 				children.push(
@@ -97,16 +99,16 @@ module.exports = (Plugin, Api) => {
 
 		onStart() {
 			try {
-				PluginUtilities.addStyle(this.getName(), css);
+				DOM.addStyle(this.name, css);
 				this.patchUserBannerMask();
 			} catch (e) {
-				Logger.err(e);
+				console.error(e);
 			}
 		}
 
 		onStop() {
-			PluginUtilities.removeStyle(this.getName());
-			Patcher.unpatchAll();
+			DOM.removeStyle(this.name);
+			Patcher.unpatchAll(this.name);
 		}
 	};
 };
