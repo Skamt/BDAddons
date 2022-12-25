ba179fe7f7562dc3f0d6b3e21d1d551c4fd0006e
Skamt <114232893+Skamt@users.noreply.github.com>
2022-12-26 00:44:59 +0100

    Untill plugin settings is added natively, For now a minor setting is added in respons to the demand

--- src/plugins/StickerEmojiPreview/index.js
+++ src/plugins/StickerEmojiPreview/index.js
@@ -10,27 +10,40 @@ module.exports = () => {
 	// Modules
 	const Popout = DiscordModules.Popout;
 	const ExpressionPickerInspector = DiscordModules.ExpressionPickerInspector;
+	const SwitchRow = DiscordModules.SwitchRow;
 
 	// Constants
 	const PREVIEW_SIZE = 300;
 
 	// components
 	const previewComponent = require("components/PreviewComponent.jsx");
-	
+
+	const settingComponent = (props) => {
+		const [enabled, setEnabled] = useState(props.value);
+		return React.createElement(SwitchRow, {
+			value: enabled,
+			onChange: e => {
+				props.onChange(e);
+				setEnabled(e);
+			}
+		}, props.description);
+	}
 	// styles
 	const css = require("styles.css");
 
 	return class StickerEmojiPreview {
 		start() {
 			try {
+				this.settings = BdApi.loadData(config.info.name, "settings") || { previewDefaultState: false };
 				DOM.addStyle(config.info.name, css);
 				Patcher.after(config.info.name, ExpressionPickerInspector, "Z", (_, [{ graphicPrimary }], ret) => {
 					return React.createElement(previewComponent, {
 						previewSize: PREVIEW_SIZE,
 						target: ret,
-						previewComponent : React.createElement(graphicPrimary.type,{
+						defaultState: this.settings.previewDefaultState,
+						previewComponent: React.createElement(graphicPrimary.type, {
 							...graphicPrimary.props,
-							src : graphicPrimary.props.src?.replace(/([?&]size=)(\d+)/, `$1${PREVIEW_SIZE}`),
+							src: graphicPrimary.props.src?.replace(/([?&]size=)(\d+)/, `$1${PREVIEW_SIZE}`),
 							disableAnimation: false,
 							size: PREVIEW_SIZE
 						})
@@ -44,6 +57,17 @@ module.exports = () => {
 		stop() {
 			DOM.removeStyle(config.info.name);
 			Patcher.unpatchAll(config.info.name);
+		}
+
+		getSettingsPanel() {
+			return React.createElement(settingComponent, {
+				description: "Preview open by default.",
+				value: this.settings.previewDefaultState,
+				onChange: e => {
+					this.settings.previewDefaultState = e;
+					BdApi.saveData(config.info.name, "settings", this.settings);
+				}
+			});
 		}
 	};
 }
