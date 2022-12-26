0877a7f92479d8a5554b0591195459657d39c854
Skamt <114232893+Skamt@users.noreply.github.com>
2022-12-26 12:57:14 +0100

    Untill plugin settings is added natively, For now a minor setting is added in respons to the demand.

--- StickerEmojiPreview/StickerEmojiPreview.plugin.js
+++ StickerEmojiPreview/StickerEmojiPreview.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name StickerEmojiPreview
  * @description Adds a zoomed preview to those tiny Stickers and Emojis
- * @version 1.0.0
+ * @version 1.0.1
  * @author Skamt
  * @website https://github.com/Skamt/BDAddons/tree/main/StickerEmojiPreview
  * @source https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/StickerEmojiPreview.plugin.js
@@ -9,7 +9,7 @@
 const config = {
 	info: {
 		name: "StickerEmojiPreview",
-		version: "1.0.0",
+		version: "1.0.1",
 		description: "Adds a zoomed preview to those tiny Stickers and Emojis",
 		source: "https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/StickerEmojiPreview.plugin.js",
 		github: "https://github.com/Skamt/BDAddons/tree/main/StickerEmojiPreview",
@@ -29,11 +29,12 @@ module.exports = (() => {
 	// Modules
 	const Popout = getModule(Filters.byStrings('renderPopout', 'animationPosition'), { searchExports: true });
 	const ExpressionPickerInspector = getModule((m) => m.Z && m.Z.toString().includes('EMOJI_IS_FAVORITE_ARIA_LABEL'));
+	const SwitchRow = getModule(m => m.toString().includes('helpdeskArticleId'));
 	// Constants
 	const PREVIEW_SIZE = 300;
 	// components
-	const previewComponent = ({ target, previewComponent, previewSize }) => {
-		const [show, setShow] = useState(false);
+	const previewComponent = ({ target, previewComponent, previewSize, defaultState }) => {
+		const [show, setShow] = useState(defaultState);
 		useEffect(() => {
 			function keyupHandler(e) {
 				if (e.key === "Control")
@@ -58,6 +59,17 @@ module.exports = (() => {
 				},
 				() => target));
 	};
+	const settingComponent = (props) => {
+		const [enabled, setEnabled] = useState(props.value);
+		return React.createElement(SwitchRow, {
+			children: props.description,
+			value: enabled,
+			onChange: e => {
+				props.onChange(e);
+				setEnabled(e);
+			}
+		});
+	}
 	// styles
 	const css = `.stickersPreview {
 	width:400px;
@@ -79,11 +91,13 @@ module.exports = (() => {
 	return class StickerEmojiPreview {
 		start() {
 			try {
+				this.settings = BdApi.loadData(config.info.name, "settings") || { previewDefaultState: false };
 				DOM.addStyle(config.info.name, css);
 				Patcher.after(config.info.name, ExpressionPickerInspector, "Z", (_, [{ graphicPrimary }], ret) => {
 					return React.createElement(previewComponent, {
 						previewSize: PREVIEW_SIZE,
 						target: ret,
+						defaultState: this.settings.previewDefaultState,
 						previewComponent: React.createElement(graphicPrimary.type, {
 							...graphicPrimary.props,
 							src: graphicPrimary.props.src?.replace(/([?&]size=)(\d+)/, `$1${PREVIEW_SIZE}`),
@@ -99,6 +113,16 @@ module.exports = (() => {
 		stop() {
 			DOM.removeStyle(config.info.name);
 			Patcher.unpatchAll(config.info.name);
+		}
+		getSettingsPanel() {
+			return React.createElement(settingComponent, {
+				description: "Preview open by default.",
+				value: this.settings.previewDefaultState,
+				onChange: e => {
+					this.settings.previewDefaultState = e;
+					BdApi.saveData(config.info.name, "settings", this.settings);
+				}
+			});
 		}
 	};
 })();
