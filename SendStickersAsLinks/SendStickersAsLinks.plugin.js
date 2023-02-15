6fe6deb79d203ab4d4e5dcc8fb1781c42bc16dda
Skamt <114232893+Skamt@users.noreply.github.com>
2023-02-15 14:52:09 +0100

    no message

--- SendStickersAsLinks/SendStickersAsLinks.plugin.js
+++ SendStickersAsLinks/SendStickersAsLinks.plugin.js
@@ -62,7 +62,23 @@ function initPlugin([Plugin, Api]) {
 				getModule
 			}
 		} = new BdApi(config.info.name);
+		// Helper functions
+		const Utils = {
+			isTagged: (str) => Object.values(TAGS).some(tag => str.includes(tag)),
+			showToast: (content, type) => UI.showToast(`[${config.info.name}] ${content}`, { type }),
+			getStickerUrl: (stickerId, size) => `https://media.discordapp.net/stickers/${stickerId}?size=${size}&passthrough=false`,
+			hasEmbedPerms: (channel, user) => !channel.guild_id || Permissions.can({ permission: DiscordPermissions.EMBED_LINKS, context: channel, user }),
+			isLottieSticker: sticker => sticker.type === StickerTypeEnum.STANDARD,
+			isAnimatedSticker: sticker => sticker["format_type"] === StickerFormatEnum.APNG,
+			isStickerSendable: (sticker, channel, user) => getStickerSendability(sticker, user, channel) === StickersSendabilityEnum.SENDABLE,
+			getModuleAndKey(filter) {
+				let module;
+				const target = BdApi.Webpack.getModule((entry, m) => filter(entry) ? (module = m) : false, { searchExports: true })
+				return [module.exports, Object.keys(module.exports).find(k => module.exports[k] === target)];
+			}
+		};
 		// Modules
+		const [StickerModule, StickerModulePatchTarget] = Utils.getModuleAndKey(Filters.byStrings('sticker', 'withLoadingIndicator'));
 		const PendingReplyStore = getModule(m => m.getPendingReply);
 		const Permissions = getModule(Filters.byProps('computePermissions'));
 		const ChannelStore = getModule(Filters.byProps('getChannel', 'getDMFromUserId'));
@@ -113,26 +129,26 @@ function initPlugin([Plugin, Api]) {
 			missingEmbedPermissionsErrorMessage: "Missing Embed Permissions",
 			disabledAnimatedStickersErrorMessage: "You have disabled animated stickers in settings."
 		};
-		// Helper functions
-		const Utils = {
-			isTagged: (str) => Object.values(TAGS).some(tag => str.includes(tag)),
-			showToast: (content, type) => UI.showToast(`[${config.info.name}] ${content}`, { type }),
-			getStickerUrl: (stickerId, size) => `https://media.discordapp.net/stickers/${stickerId}?size=${size}&passthrough=false`,
-			hasEmbedPerms: (channel, user) => !channel.guild_id || Permissions.can({ permission: DiscordPermissions.EMBED_LINKS, context: channel, user }),
-			updateStickers: () => StickerStore.stickerMetadata.forEach((value, key) => StickerStore.getStickerById(key)),
-			isLottieSticker: sticker => sticker.type === StickerTypeEnum.STANDARD,
-			isAnimatedSticker: sticker => sticker["format_type"] === StickerFormatEnum.APNG,
-			isStickerSendable: (sticker, channel, user) => getStickerSendability(sticker, user, channel) === StickersSendabilityEnum.SENDABLE,
-		};
 		// styles
-		const css = `/* Highlight animated stickers */
-.stickerAsset-4c7Oqy[alt$="ANIMATED_STICKER_TAG"]{
-	padding:1px;
-    border-radius: 12px;
-    box-sizing:border-box;
-    border: 2px dotted #ff8f09;
+		const css = `.animatedSticker{
+    position:relative;
 }
-`;
+
+.animatedSticker:before{
+    content:'';
+    padding:2px;
+    background:linear-gradient(-135deg, #42ff42 8%, transparent 0);
+    position:absolute;
+    width:100%;
+    height:100%;
+    top:-2px;
+    left:-2px;
+    z-index:55;
+}
+
+.stickerInspected-mwnU6w .animatedSticker:before{
+    border-radius:4px;
+}`;
 		return class SendStickersAsLinks extends Plugin {
 			constructor() {
 				super();
@@ -226,17 +242,13 @@ function initPlugin([Plugin, Api]) {
 				});
 			}
 			patchGetStickerById() {
-				/** 
-				 * this patch is for adding a tag to animated stickers
-				 * to style highlight them if setting is set to true
-				 * the sticker description gets added to the alt DOM attributes
-				 */
-				Patcher.after(StickerStore, "getStickerById", (_, args, sticker) => {
-					if (!sticker) return;
-					if (!Utils.isTagged(sticker.description || "") && !Utils.isLottieSticker(sticker) && Utils.isAnimatedSticker(sticker) && this.settings.shouldHighlightAnimated)
-						sticker.description += TAGS.ANIMATED_STICKER_TAG;
-					else if (!this.settings.shouldHighlightAnimated)
-						sticker.description = typeof(sticker.description) === 'string' ? sticker.description.replace(TAGS.ANIMATED_STICKER_TAG, "") : sticker.description;
+				Patcher.after(StickerModule, StickerModulePatchTarget, (_, args, returnValue) => {
+					const { size, sticker } = returnValue.props.children[0].props;
+					if (size === 96) {
+						if (this.settings.shouldHighlightAnimated && !Utils.isLottieSticker(sticker) && Utils.isAnimatedSticker(sticker)) {
+							returnValue.props.children[0].props.className += " animatedSticker"
+						}
+					}
 				});
 			}
 			patchStickerSuggestion() {
@@ -267,10 +279,6 @@ function initPlugin([Plugin, Api]) {
 			}
 			getSettingsPanel() {
 				const panel = this.buildSettingsPanel();
-				panel.addListener((id, checked) => {
-					if (id === "shouldHighlightAnimated")
-						Utils.updateStickers();
-				});
 				return panel.getElement();
 			}
 		};
