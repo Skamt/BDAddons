09e0b9fe044df6e0d9149e92331b76a4cac39c38
Skamt <114232893+Skamt@users.noreply.github.com>
2023-03-07 02:01:25 +0100

    Handling dead modules plus fallbacks, Added ErrorBoundary

--- CopyImageLink/CopyImageLink.plugin.js
+++ CopyImageLink/CopyImageLink.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name CopyImageLink
  * @description Adds (Copy Link) button next to (Open Original) under images
- * @version 1.0.3
+ * @version 1.0.4
  * @author Skamt
  * @website https://github.com/Skamt/BDAddons/tree/main/CopyImageLink
  * @source https://raw.githubusercontent.com/Skamt/BDAddons/main/CopyImageLink/CopyImageLink.plugin.js
@@ -9,7 +9,7 @@
 const config = {
 	info: {
 		name: "CopyImageLink",
-		version: "1.0.3",
+		version: "1.0.4",
 		description: "Adds (Copy Link) button next to (Open Original) under images",
 		source: "https://raw.githubusercontent.com/Skamt/BDAddons/main/CopyImageLink/CopyImageLink.plugin.js",
 		github: "https://github.com/Skamt/BDAddons/tree/main/CopyImageLink",
@@ -18,57 +18,85 @@ const config = {
 		}]
 	}
 };
-module.exports = (() => {
-	const {
-		UI,
-		DOM,
-		React,
-		Patcher,
-		Webpack: {
-			getModule
+
+function getPlugin() {
+	const { Modules, Plugin } = (() => {
+		const { Webpack: { Filters, getModule } } = BdApi;
+
+		// https://discord.com/channels/86004744966914048/196782758045941760/1062604534922367107
+		function getModuleAndKey(filter) {
+			let module;
+			const target = getModule((entry, m) => filter(entry) ? (module = m) : false, { searchExports: true });
+			module = module?.exports;
+			if (!module) return { module: undefined };
+			const key = Object.keys(module).find(k => module[k] === target);
+			if (!key) return undefined;
+			return { module, key };
 		}
-	} = new BdApi(config.info.name);
-	// https://discord.com/channels/86004744966914048/196782758045941760/1062604534922367107
-	function getModuleAndKey(filter) {
-		let module;
-		const target = BdApi.Webpack.getModule((entry, m) => filter(entry) ? (module = m) : false, { searchExports: true })
-		return [module.exports, Object.keys(module.exports).find(k => module.exports[k] === target)];
-	}
-	// Modules
-	const [ImageModalModule, ImageModalKey] = getModuleAndKey(m => {
-		if (!m?.toString || typeof(m?.toString) !== "function") return;
-		const strs = ["original", "maxHeight", "maxWidth", "noreferrer noopener"];
-		const funcStr = m?.toString();
-		for (const s of strs)
-			if (!funcStr.includes(s)) return false;
-		return true;
-	});
-	// Helper functions
-	const Utils = {
-		showToast: (content, type) => UI.showToast(`[${config.info.name}] ${content}`, { type }),
-		copy: (data) => {
-			DiscordNative.clipboard.copy(data);
-			Utils.showToast("Link Copied!", "success");
-		},
-		/* Stolen from Zlib until it gets added to BdApi */
-		getNestedProp: (obj, path) => path.split(".").reduce(function(ob, prop) {
-			return ob && ob[prop];
-		}, obj)
-	};
-	// components
-	const copyButton = ({ href }) => {
-		return (
-			React.createElement(React.Fragment, null,
-				React.createElement("span", { className: "copyBtnSpan" }, "|"),
-				React.createElement("a", {
-					className: "anchorUnderlineOnHover-2qPutX downloadLink-3cavAH copyBtn",
-					onClick: (_) => Utils.copy(href)
-				}, "Copy link")));
-	};
-	// styles
-	const css = `.copyBtn {
+
+		return {
+			Modules: {
+				ImageModal: {
+					module: getModuleAndKey(m => {
+						if (!m?.toString || typeof(m?.toString) !== "function") return;
+						const strs = ["original", "maxHeight", "maxWidth", "noreferrer noopener"];
+						const funcStr = m?.toString();
+						for (const s of strs)
+							if (!funcStr.includes(s)) return false;
+						return true;
+					}),
+					isBreakable: true,
+					withKey: true
+				}
+			},
+			Plugin(Modules) {
+				const {
+					UI,
+					DOM,
+					React,
+					Patcher
+				} = new BdApi(config.info.name);
+
+				// Utilities
+				const Utils = {
+					showToast: (content, type) => UI.showToast(`[${config.info.name}] ${content}`, { type }),
+					copy: (data) => {
+						DiscordNative.clipboard.copy(data);
+						Utils.showToast("Link Copied!", "success");
+					},
+					/* Stolen from Zlib until it gets added to BdApi */
+					getNestedProp: (obj, path) => path.split(".").reduce(function(ob, prop) {
+						return ob && ob[prop];
+					}, obj)
+				};
+
+				// Components
+				const CopyButtonComponent = ({ href }) => {
+					return React.createElement(React.Fragment, null,
+						React.createElement("span", { className: "copyBtnSpan" }, "|"),
+						React.createElement("a", {
+							className: "copyBtn",
+							onClick: (_) => Utils.copy(href)
+						}, "Copy link"));
+
+				};
+
+				// Styles
+				const css = `.copyBtn {
 	left: 115px;
 	white-space: nowrap;
+    position: absolute;
+    top: 100%;
+    font-size: 14px;
+    font-weight: 500;
+    color: #fff;
+    line-height: 30px;
+    transition: opacity .15s ease;
+    opacity: .5;
+}
+.copyBtn:hover{
+	opacity: 1;
+    text-decoration: underline;
 }
 
 .copyBtnSpan {
@@ -80,22 +108,76 @@ module.exports = (() => {
 	line-height: 30px;
 	opacity: 0.5;
 }`;
-	return class CopyImageLink {
-		start() {
-			try {
-				DOM.addStyle(css);
-				Patcher.after(ImageModalModule, ImageModalKey, (_, __, returnValue) => {
-					const children = Utils.getNestedProp(returnValue, "props.children");
-					const { href } = Utils.getNestedProp(returnValue, "props.children.2.props");
-					children.push(React.createElement(copyButton, { href }));
-				});
-			} catch (e) {
-				console.error(e);
+
+				return class CopyImageLink {
+					start() {
+						try {
+							DOM.addStyle(css);
+							Patcher.after(Modules.ImageModal.module, Modules.ImageModal.key, (_, __, returnValue) => {
+								const children = Utils.getNestedProp(returnValue, "props.children");
+								const { href } = Utils.getNestedProp(returnValue, "props.children.2.props");
+								children.push(React.createElement(CopyButtonComponent, { href }));
+							});
+						} catch (e) {
+							console.error(e);
+						}
+					}
+
+					stop() {
+						DOM.removeStyle();
+						Patcher.unpatchAll();
+					}
+				}
 			}
 		}
-		stop() {
-			DOM.removeStyle();
-			Patcher.unpatchAll();
+	})();
+	return [Plugin, Modules]
+}
+
+function pluginErrorAlert(content) {
+	BdApi.alert(config.info.name, content);
+}
+
+function getErrorPlugin(message) {
+	return () => ({
+		stop() {},
+		start() {
+			pluginErrorAlert(message);
 		}
-	};
-})();
+	})
+}
+
+function checkModules(Modules) {
+	return Object.entries(Modules).reduce((acc, [moduleName, { module, fallback, isBreakable, withKey }]) => {
+		if ((withKey && !module.module) || !module) {
+			if (isBreakable) acc[0] = true;
+			acc[2].push(moduleName);
+			if (fallback) acc[1][moduleName] = fallback;
+		} else
+			acc[1][moduleName] = module;
+		return acc;
+	}, [false, {},
+		[]
+	]);
+}
+
+function initPlugin() {
+	const [Plugin, Modules] = getPlugin();
+
+	const [pluginBreakingError, SafeModules, BrokenModules] = checkModules(Modules);
+	if (pluginBreakingError)
+		return getErrorPlugin([
+			"**Plugin is broken:** Take a screenshot of this popup and show it to the dev.",
+			`\`\`\`md\nMissing modules:\n\n${BrokenModules.map((moduleName,i) => `${++i}. ${moduleName}`).join('\n')}\`\`\``
+		]);
+	else {
+		if (BrokenModules.length > 0)
+			pluginErrorAlert([
+				"Detected some Missing modules, certain aspects of the plugin may not work properly.",
+				`\`\`\`md\n[Missing modules]\n\n${BrokenModules.map((moduleName,i) => `${++i}. ${moduleName}`).join('\n')}\`\`\``
+			]);
+		return Plugin(SafeModules);
+	}
+}
+
+module.exports = initPlugin();
--- ViewProfilePicture/ViewProfilePicture.plugin.js
+++ ViewProfilePicture/ViewProfilePicture.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name ViewProfilePicture
  * @description Adds a button to the user popout and profile that allows you to view the Avatar and banner.
- * @version 1.0.4
+ * @version 1.0.5
  * @author Skamt
  * @website https://github.com/Skamt/BDAddons/tree/main/ViewProfilePicture
  * @source https://raw.githubusercontent.com/Skamt/BDAddons/main/ViewProfilePicture/ViewProfilePicture.plugin.js
@@ -9,7 +9,7 @@
 const config = {
 	info: {
 		name: "ViewProfilePicture",
-		version: "1.0.4",
+		version: "1.0.5",
 		description: "Adds a button to the user popout and profile that allows you to view the Avatar and banner.",
 		source: "https://raw.githubusercontent.com/Skamt/BDAddons/main/ViewProfilePicture/ViewProfilePicture.plugin.js",
 		github: "https://github.com/Skamt/BDAddons/tree/main/ViewProfilePicture",
@@ -18,123 +18,169 @@ const config = {
 		}]
 	}
 };
-module.exports = (() => {
-	const {
-		UI,
-		DOM,
-		React,
-		Patcher,
-		Webpack: {
-			Filters,
-			getModule
+
+function getPlugin() {
+	const { Modules, Plugin } = (() => {
+		const { Webpack: { Filters, getModule } } = BdApi
+
+		// https://discord.com/channels/86004744966914048/196782758045941760/1062604534922367107
+		function getModuleAndKey(filter) {
+			let module;
+			const target = getModule((entry, m) => filter(entry) ? (module = m) : false, { searchExports: true });
+			module = module?.exports;
+			if (!module) return { module: undefined };
+			const key = Object.keys(module).find(k => module[k] === target);
+			if (!key) return undefined;
+			return { module, key };
 		}
-	} = new BdApi(config.info.name);
 
-	// https://discord.com/channels/86004744966914048/196782758045941760/1062604534922367107
-	function getModuleAndKey(filter) {
-		let module;
-		const target = BdApi.Webpack.getModule((entry, m) => filter(entry) ? (module = m) : false, { searchExports: true })
-		return [module.exports, Object.keys(module.exports).find(k => module.exports[k] === target)];
-	}
-
-	// Modules
-	const Tooltip = getModule(m => m.defaultProps?.shouldShow, { searchExports: true });
-	const ModalRoot = getModule(Filters.byStrings('onAnimationEnd'), { searchExports: true });
-	const openModal = getModule(Filters.byStrings('onCloseCallback', 'Layer'), { searchExports: true });
-	const [ImageModalModule, ImageModalKey] = getModuleAndKey(m => {
-		if (!m?.toString || typeof(m?.toString) !== "function") return;
-		const strs = ["original", "maxHeight", "maxWidth", "noreferrer noopener"];
-		const funcStr = m?.toString();
-		for (const s of strs)
-			if (!funcStr.includes(s)) return false;
-		return true;
-	});
-	const ModalCarousel = getModule(m => m.prototype?.navigateTo && m.prototype?.preloadImage);
-	const UserBannerMask = getModule((m) => m.Z && m.Z.toString().includes('overrideAvatarDecorationURL'));
-	const ProfileTypeEnum = getModule(Filters.byProps('POPOUT', 'SETTINGS'), { searchExports: true });
-	const CurrentUserStore = getModule(Filters.byProps('getCurrentUser', 'getUsers'));
-	const SelectedGuildStore = getModule(Filters.byProps('getLastSelectedGuildId'));
-	const renderLinkComponent = getModule(m => m.type?.toString().includes('MASKED_LINK'));
-
-	// Constants
-	const IMG_WIDTH = 4096;
-
-	// Helper functions
-	const Utils = {
-		showToast: (content, type) => UI.showToast(`[${config.info.name}] ${content}`, { type }),
-		copy: (data) => {
-			DiscordNative.clipboard.copy(data);
-			Utils.showToast("Color Copied!", "success");
-		},
-		/* Stolen from Zlib until it gets added to BdApi */
-		getNestedProp: (obj, path) => path.split(".").reduce(function(ob, prop) {
-			return ob && ob[prop];
-		}, obj),
-		getImageModalComponent: (Url, props) => React.createElement(ImageModalModule[ImageModalKey], {
-			...props,
-			src: Url,
-			original: Url,
-			renderLinkComponent: p => React.createElement(renderLinkComponent, p)
-		})
-	};
-
-	// Components
-	const ViewProfilePictureButton = (props) => {
-		return (
-			React.createElement(Tooltip, {
-					text: "View profile picture",
-					position: "top"
+		return {
+			Modules: {
+				Tooltip: { module: getModule(m => m.defaultProps?.shouldShow, { searchExports: true }), isBreakable: true },
+				ModalRoot: { module: getModule(Filters.byStrings('onAnimationEnd'), { searchExports: true }), isBreakable: true },
+				openModal: { module: getModule(Filters.byStrings('onCloseCallback', 'Layer'), { searchExports: true }), isBreakable: true },
+				ImageModal: {
+					module: getModuleAndKey(m => {
+						if (!m?.toString || typeof(m?.toString) !== "function") return;
+						const strs = ["original", "maxHeight", "maxWidth", "noreferrer noopener"];
+						const funcStr = m?.toString();
+						for (const s of strs)
+							if (!funcStr.includes(s)) return false;
+						return true;
+					}),
+					isBreakable: true,
+					withKey: true
 				},
-				(p) =>
-				React.createElement("div", {
-						...
-						p,
-						...
-						props
+				ModalCarousel: { module: getModule(m => m.prototype?.navigateTo && m.prototype?.preloadImage), isBreakable: true },
+				UserBannerMask: { module: getModule((m) => m.Z && m.Z.toString().includes('overrideAvatarDecorationURL')), isBreakable: true },
+				ProfileTypeEnum: { module: getModule(Filters.byProps('POPOUT', 'SETTINGS'), { searchExports: true }), fallback: { POPOUT: 0, MODAL: 1, SETTINGS: 2, PANEL: 3, CARD: 4 } },
+				CurrentUserStore: { module: getModule(Filters.byProps('getCurrentUser', 'getUsers')), isBreakable: true },
+				SelectedGuildStore: { module: getModule(Filters.byProps('getLastSelectedGuildId')) },
+				renderLinkComponent: { module: getModule(m => m.type?.toString().includes('MASKED_LINK')), isBreakable: true }
+			},
+			Plugin(Modules) {
+				const {
+					UI,
+					DOM,
+					React,
+					Patcher
+				} = new BdApi(config.info.name);
+				// Utilities
+				const Utils = {
+					showToast: (content, type) => UI.showToast(`[${config.info.name}] ${content}`, { type }),
+					copy: (data) => {
+						DiscordNative.clipboard.copy(data);
+						Utils.showToast("Color Copied!", "success");
 					},
-					React.createElement("svg", {
-							"aria-label": p["aria-label"],
-							"aria-hidden": "false",
-							role: "img",
-							width: "18",
-							height: "18",
-							viewBox: "-50 -50 484 484"
-						},
-						React.createElement("path", {
-							fill: "currentColor",
-							d: "M341.333,0H42.667C19.093,0,0,19.093,0,42.667v298.667C0,364.907,19.093,384,42.667,384h298.667 C364.907,384,384,364.907,384,341.333V42.667C384,19.093,364.907,0,341.333,0z M42.667,320l74.667-96l53.333,64.107L245.333,192l96,128H42.667z"
-						})))));
+					/* Stolen from Zlib until it gets added to BdApi */
+					getNestedProp: (obj, path) => path.split(".").reduce(function(ob, prop) {
+						return ob && ob[prop];
+					}, obj),
+					getImageModalComponent: (Url, props) => React.createElement(Modules.ImageModal.module[Modules.ImageModal.key], {
+						...props,
+						src: Url,
+						original: Url,
+						renderLinkComponent: p => React.createElement(Modules.renderLinkComponent, p)
+					})
+				};
 
-	};
-	const DisplayCarousel = ({ props, items }) => {
-		return (
-			React.createElement(ModalRoot, {
-					...
-					props,
-					className: "VPP-carousel carouselModal-1eUFoq zoomedCarouselModalRoot-beLNhM"
-				},
-				React.createElement(ModalCarousel, {
-					startWith: 0,
-					className: "modalCarouselWrapper-YK1MX4",
-					items: items.map((item) => ({ "component": item }))
-				})));
+				// Constants
+				const IMG_WIDTH = 4096;
 
-	};
-	const ColorModal = ({ color, bannerColorCopyHandler }) => {
-		return (
-			React.createElement("div", {
-					className: "VPP-NoBanner wrapper-2bCXfR",
-					style: { backgroundColor: color }
-				},
-				React.createElement("a", {
-					className: "anchorUnderlineOnHover-2qPutX downloadLink-3cavAH",
-					onClick: (_) => Utils.copy(color)
-				}, "Copy Color")));
+				// Components
+				const ErrorBoundary = class ErrorBoundary extends React.Component {
+					state = { hasError: false, error: null, info: null };
 
-	};
+					componentDidCatch(error, info) {
+						this.setState({ error, info, hasError: true });
+						const errorMessage = `\n\t${error?.message || ""}${(info?.componentStack || "").split("\n").slice(0, 20).join("\n")}`;
+						console.error(`%c[${this.props.plugin}] %cthrew an exception at %c[${this.props.id}]\n`, "color: #3a71c1;font-weight: bold;", "", "color: red;font-weight: bold;", errorMessage);
+					}
 
-	// styles
-	const css = `/* Warning circle in popouts of users who left server overlaps VPP button */
+					render() {
+						if (this.state.hasError) {
+							if (this.props.fallback) return this.props.fallback;
+							else {
+								return (
+									React.createElement("div", { style: { background: "#292c2c", padding: "20px", borderRadius: "10px" } },
+										React.createElement("b", { style: { color: "#e0e1e5" } }, "An error has occured while rendering ",
+											React.createElement("span", { style: { color: "orange" } }, this.props.id))));
+
+							}
+						} else return this.props.children;
+					}
+				};
+				const ErrorComponent = (props) =>
+					React.createElement("div", { ...props },
+						React.createElement("svg", {
+								xmlns: "http://www.w3.org/2000/svg",
+								viewBox: "0 0 24 24",
+								fill: "red",
+								width: "18",
+								height: "18"
+							},
+							React.createElement("path", {
+								d: "M0 0h24v24H0z",
+								fill: "none"
+							}),
+
+							React.createElement("path", { d: "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" })));
+				const ViewProfilePictureButtonComponent = (props) => {
+					return (
+						React.createElement(Modules.Tooltip, {
+								text: "View profile picture",
+								position: "top"
+							},
+							(p) =>
+							React.createElement("div", {
+									...
+									p,
+									...
+									props
+								},
+								React.createElement("svg", {
+										"aria-label": p["aria-label"],
+										"aria-hidden": "false",
+										role: "img",
+										width: "18",
+										height: "18",
+										viewBox: "-50 -50 484 484"
+									},
+									React.createElement("path", {
+										fill: "currentColor",
+										d: "M341.333,0H42.667C19.093,0,0,19.093,0,42.667v298.667C0,364.907,19.093,384,42.667,384h298.667 C364.907,384,384,364.907,384,341.333V42.667C384,19.093,364.907,0,341.333,0z M42.667,320l74.667-96l53.333,64.107L245.333,192l96,128H42.667z"
+									})))));
+
+				};
+				const DisplayCarouselComponent = ({ props, items }) => {
+					return (
+						React.createElement(Modules.ModalRoot, {
+								...
+								props,
+								className: "VPP-carousel carouselModal-1eUFoq zoomedCarouselModalRoot-beLNhM"
+							},
+							React.createElement(Modules.ModalCarousel, {
+								startWith: 0,
+								className: "modalCarouselWrapper-YK1MX4",
+								items: items.map((item) => ({ "component": item }))
+							})));
+
+				};
+				const ColorModalComponent = ({ color, bannerColorCopyHandler }) => {
+					return (
+						React.createElement("div", {
+								className: "VPP-NoBanner",
+								style: { backgroundColor: color }
+							},
+							React.createElement("a", {
+								className: "copyColorBtn",
+								onClick: (_) => Utils.copy(color)
+							}, "Copy Color")));
+
+				};
+
+				// Styles
+				const css = `/* Warning circle in popouts of users who left server overlaps VPP button */
 svg.warningCircleIcon-2osUEe {
     top: 75px;
 }
@@ -222,65 +268,146 @@ svg.warningCircleIcon-2osUEe {
 
 .VPP-carousel .imageWrapper-oMkQl4 > img {
 	max-height: 80vh;
+}
+
+/* Copy color button */
+.copyColorBtn{
+	white-space: nowrap;
+    position: absolute;
+    top: 100%;
+    font-size: 14px;
+    font-weight: 500;
+    color: #fff;
+    line-height: 30px;
+    transition: opacity .15s ease;
+    opacity: .5;
+}
+
+.copyColorBtn:hover{
+	opacity: 1;
+    text-decoration: underline;
 }`;
 
-	return class ViewProfilePicture {
-		constructor() {}
+				return class ViewProfilePicture {
 
-		openCarousel(items) {
-			openModal(props => React.createElement(DisplayCarousel, { props, items }));
-		}
+					openCarousel(items) {
+						Modules.openModal(props =>
+							React.createElement(ErrorBoundary, {
+									id: "DisplayCarouselComponent",
+									plugin: config.info.name,
+									closeModal: props.onClose
+								},
+								React.createElement(DisplayCarouselComponent, { props, items }))
+						);
+					}
 
-		clickHandler(user, bannerObject, isUserPopout) {
-			const { backgroundColor, backgroundImage } = bannerObject;
-			const guildId = isUserPopout ? SelectedGuildStore.getGuildId() : "";
-			const avatarURL = user.getAvatarURL(guildId, IMG_WIDTH, true);
-			const AvatarImageComponent = Utils.getImageModalComponent(avatarURL, { width: IMG_WIDTH, height: IMG_WIDTH });
-			const BannerImageComponent = backgroundImage ?
-				Utils.getImageModalComponent(`${backgroundImage.match(/(?<=\().*(?=\?)/)?.[0]}?size=${IMG_WIDTH}`, { width: IMG_WIDTH }) :
-				React.createElement(ColorModal, { color: backgroundColor });
-			this.openCarousel([AvatarImageComponent, BannerImageComponent]);
-		}
+					clickHandler(user, bannerObject, isUserPopout) {
+						const { backgroundColor, backgroundImage } = bannerObject;
+						const guildId = isUserPopout ? Modules.SelectedGuildStore.getGuildId() : "";
+						const avatarURL = user.getAvatarURL(guildId, IMG_WIDTH, true);
+						const AvatarImageComponent = Utils.getImageModalComponent(avatarURL, { width: IMG_WIDTH, height: IMG_WIDTH });
+						const BannerImageComponent = backgroundImage ?
+							Utils.getImageModalComponent(`${backgroundImage.match(/(?<=\().*(?=\?)/)?.[0]}?size=${IMG_WIDTH}`, { width: IMG_WIDTH }) :
+							React.createElement(ColorModalComponent, { color: backgroundColor });
+						this.openCarousel([AvatarImageComponent, BannerImageComponent]);
+					}
 
-		patchUserBannerMask() {
-			Patcher.after(UserBannerMask, "Z", (_, [{ user, isPremium, profileType }], returnValue) => {
-				if (profileType === ProfileTypeEnum.SETTINGS) return;
+					patchUserBannerMask() {
+						Patcher.after(Modules.UserBannerMask, "Z", (_, [{ user, isPremium, profileType }], returnValue) => {
+							if (profileType === Modules.ProfileTypeEnum.SETTINGS) return;
 
-				const currentUser = CurrentUserStore.getCurrentUser();
-				let className = "VPP-Button";
-				if (profileType === ProfileTypeEnum.MODAL)
-					className += " VPP-profile"
+							const currentUser = Modules.CurrentUserStore.getCurrentUser();
+							let className = "VPP-Button";
+							if (profileType === Modules.ProfileTypeEnum.MODAL)
+								className += " VPP-profile"
 
-				const bannerObject = Utils.getNestedProp(returnValue, "props.children.1.props.children.props.style");
-				const children = Utils.getNestedProp(returnValue, "props.children.1.props.children.props.children");
-				className += user.id === currentUser.id ?
-					" VPP-self" :
-					bannerObject.backgroundImage ?
-					" VPP-left" :
-					" VPP-right";
+							const bannerObject = Utils.getNestedProp(returnValue, "props.children.1.props.children.props.style");
+							const children = Utils.getNestedProp(returnValue, "props.children.1.props.children.props.children");
+							className += user.id === currentUser.id ?
+								" VPP-self" :
+								bannerObject.backgroundImage ?
+								" VPP-left" :
+								" VPP-right";
 
-				if (Array.isArray(children) && bannerObject)
-					children.push(
-						React.createElement(ViewProfilePictureButton, {
-							className,
-							onClick: _ => this.clickHandler(user, bannerObject, ProfileTypeEnum.POPOUT === profileType)
-						})
-					);
-			});
-		}
+							if (Array.isArray(children) && bannerObject)
+								children.push(
+									React.createElement(ErrorBoundary, {
+											id: "ViewProfilePictureButtonComponent",
+											plugin: config.info.name,
+											fallback: React.createElement(ErrorComponent, { className })
+										},
+										React.createElement(ViewProfilePictureButtonComponent, {
+											className,
+											onClick: _ => this.clickHandler(user, bannerObject, Modules.ProfileTypeEnum.POPOUT === profileType)
+										}))
+								);
+						});
+					}
 
-		start() {
-			try {
-				DOM.addStyle(css);
-				this.patchUserBannerMask();
-			} catch (e) {
-				console.error(e);
+					start() {
+						try {
+							DOM.addStyle(css);
+							this.patchUserBannerMask();
+						} catch (e) {
+							console.error(e);
+						}
+					}
+
+					stop() {
+						DOM.removeStyle();
+						Patcher.unpatchAll();
+					}
+				}
 			}
 		}
+	})();
+	return [Plugin, Modules]
+}
 
-		stop() {
-			DOM.removeStyle();
-			Patcher.unpatchAll();
+function pluginErrorAlert(content) {
+	BdApi.alert(config.info.name, content);
+}
+
+function getErrorPlugin(message) {
+	return () => ({
+		stop() {},
+		start() {
+			pluginErrorAlert(message);
 		}
-	};
-})();
+	})
+}
+
+function checkModules(Modules) {
+	return Object.entries(Modules).reduce((acc, [moduleName, { module, fallback, isBreakable, withKey }]) => {
+		if ((withKey && !module.module) || !module) {
+			if (isBreakable) acc[0] = true;
+			acc[2].push(moduleName);
+			if (fallback) acc[1][moduleName] = fallback;
+		} else
+			acc[1][moduleName] = module;
+		return acc;
+	}, [false, {},
+		[]
+	]);
+}
+
+function initPlugin() {
+	const [Plugin, Modules] = getPlugin();
+
+	const [pluginBreakingError, SafeModules, BrokenModules] = checkModules(Modules);
+	if (pluginBreakingError)
+		return getErrorPlugin([
+			"**Plugin is broken:** Take a screenshot of this popup and show it to the dev.",
+			`\`\`\`md\nMissing modules:\n\n${BrokenModules.map((moduleName,i) => `${++i}. ${moduleName}`).join('\n')}\`\`\``
+		]);
+	else {
+		if (BrokenModules.length > 0)
+			pluginErrorAlert([
+				"Detected some Missing modules, certain aspects of the plugin may not work properly.",
+				`\`\`\`md\n[Missing modules]\n\n${BrokenModules.map((moduleName,i) => `${++i}. ${moduleName}`).join('\n')}\`\`\``
+			]);
+		return Plugin(SafeModules);
+	}
+}
+
+module.exports = initPlugin();
