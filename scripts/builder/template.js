c9ed628652a1ded4dbe420362f516aed97c96bfa
Skamt <114232893+Skamt@users.noreply.github.com>
2023-03-17 20:09:47 +0100

    no message

--- scripts/builder/template.js
+++ scripts/builder/template.js
@@ -3,15 +3,237 @@ const config = {{ PLUGIN__CONFIG }};
 
 {{ PLUGIN__BODY }}
 
-function pluginErrorAlert(content) {
-	BdApi.alert(config.info.name, content);
+const BM_CSS = `.bm-container.closing .backdrop{
+	animation:hide-backdrop 300ms ease-in;
+}
+
+#bm-container .backdrop{
+	animation:show-backdrop 300ms ease-out;
+}
+
+@keyframes show-backdrop {
+	from{ opacity:0; }
+	to{ opacity:.85; }
+}
+
+@keyframes hide-backdrop {
+	from{ opacity:.85; }
+	to{ opacity:0;}
+}
+
+
+#bm-container .modal{
+	animation:show-modal 300ms ease-out;
+}
+#bm-container.closing .modal{
+	animation:hide-modal 300ms ease-in;
+}
+
+@keyframes show-modal {
+	from{
+		transform: scale(0);
+		opacity:0;
+	}
+	to{
+		opacity:.85;
+		transform: scale(1);
+	}
+}
+
+@keyframes hide-modal {
+	from{
+		opacity:.85;
+		transform: scale(1);
+	}
+	to{
+		transform: scale(0);
+		opacity:0;
+	}
+}
+
+#bm-container{
+	position: fixed;
+	z-index:999999;
+	top: 0;
+	left: 0;
+	right:0;
+	bottom:0;
+	display:flex;
+	overflow:hidden;
+}
+
+
+#bm-container .backdrop{
+	background: var(--black-500);
+	position: absolute;
+	z-index:-1;
+	top: 0;
+	left: 0;
+	right:0;
+	bottom:0;
+	opacity:.85;
+}
+
+#bm-container .modal {
+	display: inline-flex;
+	flex-direction: column;
+	background: #57616f;
+	color: white;
+	overflow: hidden;
+	border-radius: 8px;
+	margin:auto;
+	max-width:500px;
+	max-height: 60vh;
+}
+
+#bm-container .head {
+	background: #25272a;
+	padding: 20px;
+}
+
+#bm-container .head > h2{
+	font-size:1.5rem;
+	font-weight: bold;
+}
+
+#bm-container .body {
+	background: #313437;
+	padding: 10px;
+	display:flex;
+	flex-direction:column;
+	min-height:0;
+}
+
+
+#bm-container .note{
+	color: var(--text-normal);
+	font-size: 1rem;
+	line-height: 20px;
+}
+
+#bm-container .bm{
+	margin:10px 0;
+	text-decoration:underline;
+	font-size: 1.30em;
+	font-weight: bold;
+}
+
+#bm-container .modules{
+	padding:10px;
+	display:flex;
+	flex-direction:column;
+	overflow:hidden auto;
+}
+
+#bm-container .modules::-webkit-scrollbar { width: 2px;}
+#bm-container .modules::-webkit-scrollbar-thumb { background-color:orange;}
+
+#bm-container .module{
+	margin:5px;
+	padding:5px;
+	background:#27292b;
+	border-radius: 5px;
+	display:flex;
+}
+
+#bm-container .num{
+	align-self:center;
+	padding:5px;
+	margin-right:5px;
+	border-right:3px solid #8b8b8b;
+	font-size: 1.5em;
+	font-weight: bold;
+}
+
+
+#bm-container .name{
+	display: block;
+	line-height: 24px;
+	font-size: 16px;
+	font-weight: 700;
+	color: var(--header-primary);
+}
+
+#bm-container .errormessage{
+	margin:0;
+	font-size: 13px;
+	line-height: 20px;
+	color: var(--header-secondary);
+}
+
+#bm-container .footer {
+	background: #25272a;
+	padding: 10px;
+	display:flex;
+}
+
+#bm-container button{
+	margin-left:auto;
+	border-radius: 3px;
+	border: none;
+	min-width: 96px;
+	min-height: 38px;
+	width: auto;
+	color: var(--white-500);
+	background-color: var(--brand-experiment);
+}
+
+#bm-container button:hover {
+	background-color: var(--brand-experiment-560);
+}
+
+#bm-container button:active {
+	background-color: var(--brand-experiment-600);
+}
+#bm-container.hide{
+	display:none;
+}`
+
+function showBrokenModulesAlert(modules) {
+	if(!document.querySelector('head > bd-head > bd-styles > #bm'))
+		BdApi.DOM.addStyle('bm',BM_CSS);
+	
+	const template = document.createElement("template");
+	template.innerHTML = `<div id="bm-container">
+							<div class="backdrop"></div>
+							<div class="modal">
+								<div class="head">
+									<h2>${config.info.name}</h2>
+								</div>
+								<div class="body">
+									<p class="note">Detected some Missing modules, certain aspects of the plugin may not work properly.</p>
+									<h3 class="bm">Broken Modules:</h3>
+									<div class="modules">
+										${modules.map(([moduleName, errorNote],i) => `<div class="module">
+											<h2 class="num">${++i}</h2>
+											<div>
+												<h3 class="name">${moduleName}</h3>
+												<p class="errormessage">${errorNote}</p>
+											</div>
+										</div>`).join('')}
+									</div>
+								</div>
+								<div class="footer">
+									<button class="close-btn">Close</button>
+								</div>
+							</div>
+						</div>`;
+
+	const modal = template.content.firstElementChild.cloneNode(true);
+	modal.onclick = (e) => {
+		if(e.target.classList.contains('close-btn') || e.target.classList.contains('backdrop')){
+			modal.classList.add("closing");
+			setTimeout(()=>{modal.remove();},150);
+		}
+	};
+	document.querySelector('bd-body').append(modal);
 }
 
 function getErrorPlugin(message) {
 	return () => ({
 		stop() {},
 		start() {
-			pluginErrorAlert(message);
+			BdApi.alert(config.info.name, message);
 		}
 	})
 }
@@ -25,9 +247,7 @@ function checkModules(modules) {
 		} else
 			acc[1][moduleName] = module;
 		return acc;
-	}, [false, {},
-		[]
-	]);
+	}, [false, {}, []]);
 }
 
 function ensuredata() {
@@ -58,15 +278,12 @@ function handleBrokenModules(brokenModules) {
 	const isUpdated = current.version != config.info.version;
 	const isPopupLimitReached = current.errorPopupCount === 3;
 
-	if (isUpdated || !isPopupLimitReached || newBrokenModules) {
-		pluginErrorAlert([
-			"Detected some Missing modules, certain aspects of the plugin may not work properly.",
-			`\`\`\`md\nMissing modules:\n\n${brokenModules.map(([moduleName, errorNote]) => `[${moduleName}]: ${errorNote || ""}`).join('\n')}\`\`\``
-		]);
+	if (isUpdated || !false || newBrokenModules) {
+		showBrokenModulesAlert(brokenModules);
 
 		BdApi.Data.save(config.info.name, 'brokenModulesData', {
 			...current,
-			version : current.version,
+			version: current.version,
 			errorPopupCount: (current.errorPopupCount + 1) % 4,
 			savedBrokenModules: brokenModules.map(([moduleName]) => moduleName)
 		});
