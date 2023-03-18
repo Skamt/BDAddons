04201c507c3656952f6847400b5617cb26531284
Skamt <114232893+Skamt@users.noreply.github.com>
2023-03-18 17:16:36 +0100

    no message

--- scripts/builder/ztemplate.js
+++ scripts/builder/ztemplate.js
@@ -3,195 +3,197 @@ const config = {{ PLUGIN__CONFIG }};
 
 {{ PLUGIN__BODY }}
 
-const BM_CSS = `.bm-container.closing .backdrop{
-	animation:hide-backdrop 300ms ease-in;
-}
-
-#bm-container .backdrop{
-	animation:show-backdrop 300ms ease-out;
-}
-
-@keyframes show-backdrop {
-	from{ opacity:0; }
-	to{ opacity:.85; }
-}
-
-@keyframes hide-backdrop {
-	from{ opacity:.85; }
-	to{ opacity:0;}
-}
-
-
-#bm-container .modal{
-	animation:show-modal 300ms ease-out;
-}
-#bm-container.closing .modal{
-	animation:hide-modal 300ms ease-in;
-}
-
-@keyframes show-modal {
-	from{
-		transform: scale(0);
-		opacity:0;
+function BM_CSS() { 
+	return `.bm-container.closing .backdrop{
+		animation:hide-backdrop 300ms ease-in;
 	}
-	to{
+	
+	#bm-container .backdrop{
+		animation:show-backdrop 300ms ease-out;
+	}
+	
+	@keyframes show-backdrop {
+		from{ opacity:0; }
+		to{ opacity:.85; }
+	}
+	
+	@keyframes hide-backdrop {
+		from{ opacity:.85; }
+		to{ opacity:0;}
+	}
+	
+	
+	#bm-container .modal{
+		animation:show-modal 300ms ease-out;
+	}
+	#bm-container.closing .modal{
+		animation:hide-modal 300ms ease-in;
+	}
+	
+	@keyframes show-modal {
+		from{
+			transform: scale(0);
+			opacity:0;
+		}
+		to{
+			opacity:.85;
+			transform: scale(1);
+		}
+	}
+	
+	@keyframes hide-modal {
+		from{
+			opacity:.85;
+			transform: scale(1);
+		}
+		to{
+			transform: scale(0);
+			opacity:0;
+		}
+	}
+	
+	#bm-container{
+		position: fixed;
+		z-index:999999;
+		top: 0;
+		left: 0;
+		right:0;
+		bottom:0;
+		display:flex;
+		overflow:hidden;
+	}
+	
+	
+	#bm-container .backdrop{
+		background: var(--black-500);
+		position: absolute;
+		z-index:-1;
+		top: 0;
+		left: 0;
+		right:0;
+		bottom:0;
 		opacity:.85;
-		transform: scale(1);
 	}
-}
-
-@keyframes hide-modal {
-	from{
-		opacity:.85;
-		transform: scale(1);
+	
+	#bm-container .modal {
+		display: inline-flex;
+		flex-direction: column;
+		background: #57616f;
+		color: white;
+		overflow: hidden;
+		border-radius: 8px;
+		margin:auto;
+		max-width:500px;
+		max-height: 60vh;
 	}
-	to{
-		transform: scale(0);
-		opacity:0;
+	
+	#bm-container .head {
+		background: #25272a;
+		padding: 20px;
 	}
+	
+	#bm-container .head > h2{
+		font-size:1.5rem;
+		font-weight: bold;
+	}
+	
+	#bm-container .body {
+		background: #313437;
+		padding: 10px;
+		display:flex;
+		flex-direction:column;
+		min-height:0;
+	}
+	
+	
+	#bm-container .note{
+		color: var(--text-normal);
+		font-size: 1rem;
+		line-height: 20px;
+	}
+	
+	#bm-container .bm{
+		margin:10px 0;
+		text-decoration:underline;
+		font-size: 1.30em;
+		font-weight: bold;
+	}
+	
+	#bm-container .modules{
+		padding:10px;
+		display:flex;
+		flex-direction:column;
+		overflow:hidden auto;
+	}
+	
+	#bm-container .modules::-webkit-scrollbar { width: 2px;}
+	#bm-container .modules::-webkit-scrollbar-thumb { background-color:orange;}
+	
+	#bm-container .module{
+		margin:5px;
+		padding:5px;
+		background:#27292b;
+		border-radius: 5px;
+		display:flex;
+	}
+	
+	#bm-container .num{
+		align-self:center;
+		padding:5px;
+		margin-right:5px;
+		border-right:3px solid #8b8b8b;
+		font-size: 1.5em;
+		font-weight: bold;
+	}
+	
+	
+	#bm-container .name{
+		display: block;
+		line-height: 24px;
+		font-size: 16px;
+		font-weight: 700;
+		color: var(--header-primary);
+	}
+	
+	#bm-container .errormessage{
+		margin:0;
+		font-size: 13px;
+		line-height: 20px;
+		color: var(--header-secondary);
+	}
+	
+	#bm-container .footer {
+		background: #25272a;
+		padding: 10px;
+		display:flex;
+	}
+	
+	#bm-container button{
+		margin-left:auto;
+		border-radius: 3px;
+		border: none;
+		min-width: 96px;
+		min-height: 38px;
+		width: auto;
+		color: var(--white-500);
+		background-color: var(--brand-experiment);
+	}
+	
+	#bm-container button:hover {
+		background-color: var(--brand-experiment-560);
+	}
+	
+	#bm-container button:active {
+		background-color: var(--brand-experiment-600);
+	}
+	#bm-container.hide{
+		display:none;
+	}`
 }
-
-#bm-container{
-	position: fixed;
-	z-index:999999;
-	top: 0;
-	left: 0;
-	right:0;
-	bottom:0;
-	display:flex;
-	overflow:hidden;
-}
-
-
-#bm-container .backdrop{
-	background: var(--black-500);
-	position: absolute;
-	z-index:-1;
-	top: 0;
-	left: 0;
-	right:0;
-	bottom:0;
-	opacity:.85;
-}
-
-#bm-container .modal {
-	display: inline-flex;
-	flex-direction: column;
-	background: #57616f;
-	color: white;
-	overflow: hidden;
-	border-radius: 8px;
-	margin:auto;
-	max-width:500px;
-	max-height: 60vh;
-}
-
-#bm-container .head {
-	background: #25272a;
-	padding: 20px;
-}
-
-#bm-container .head > h2{
-	font-size:1.5rem;
-	font-weight: bold;
-}
-
-#bm-container .body {
-	background: #313437;
-	padding: 10px;
-	display:flex;
-	flex-direction:column;
-	min-height:0;
-}
-
-
-#bm-container .note{
-	color: var(--text-normal);
-	font-size: 1rem;
-	line-height: 20px;
-}
-
-#bm-container .bm{
-	margin:10px 0;
-	text-decoration:underline;
-	font-size: 1.30em;
-	font-weight: bold;
-}
-
-#bm-container .modules{
-	padding:10px;
-	display:flex;
-	flex-direction:column;
-	overflow:hidden auto;
-}
-
-#bm-container .modules::-webkit-scrollbar { width: 2px;}
-#bm-container .modules::-webkit-scrollbar-thumb { background-color:orange;}
-
-#bm-container .module{
-	margin:5px;
-	padding:5px;
-	background:#27292b;
-	border-radius: 5px;
-	display:flex;
-}
-
-#bm-container .num{
-	align-self:center;
-	padding:5px;
-	margin-right:5px;
-	border-right:3px solid #8b8b8b;
-	font-size: 1.5em;
-	font-weight: bold;
-}
-
-
-#bm-container .name{
-	display: block;
-	line-height: 24px;
-	font-size: 16px;
-	font-weight: 700;
-	color: var(--header-primary);
-}
-
-#bm-container .errormessage{
-	margin:0;
-	font-size: 13px;
-	line-height: 20px;
-	color: var(--header-secondary);
-}
-
-#bm-container .footer {
-	background: #25272a;
-	padding: 10px;
-	display:flex;
-}
-
-#bm-container button{
-	margin-left:auto;
-	border-radius: 3px;
-	border: none;
-	min-width: 96px;
-	min-height: 38px;
-	width: auto;
-	color: var(--white-500);
-	background-color: var(--brand-experiment);
-}
-
-#bm-container button:hover {
-	background-color: var(--brand-experiment-560);
-}
-
-#bm-container button:active {
-	background-color: var(--brand-experiment-600);
-}
-#bm-container.hide{
-	display:none;
-}`
 
 function showBrokenModulesAlert(modules) {
 	if(!document.querySelector('head > bd-head > bd-styles > #bm'))
-		BdApi.DOM.addStyle('bm',BM_CSS);
+		BdApi.DOM.addStyle('bm', BM_CSS());
 	
 	const template = document.createElement("template");
 	template.innerHTML = `<div id="bm-container">
