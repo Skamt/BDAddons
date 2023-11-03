6ecfc54de449c91266427272f64191e981879c49
Skamt <114232893+Skamt@users.noreply.github.com>
2023-11-03 16:03:00 +0100

    16:02:59.37 - Fri 11/03/2023

--- src/Emojis/patches/patchEmojiUtils.jsx
+++ src/Emojis/patches/patchEmojiUtils.jsx
@@ -5,15 +5,15 @@ import { getNestedProp, copy } from "@Utils";
 
 import Toast from "@Utils/Toast";
 import Button from "@Components/Button";
-import EmojiStore from "@Stores/EmojiStore";
 
 const MessageDecorations = getModule(Filters.byProps("MessagePopoutContent"));
+const AssetURLUtils = getModule(Filters.byProps("getEmojiURL"));
 
 export default () => {
 	if (MessageDecorations && MessageDecorations.MessagePopoutContent)
 		Patcher.after(MessageDecorations, "MessagePopoutContent", (_, __, ret) => {
-			const { emojiId } = getNestedProp(ret, "props.children.0.props.children.0.props.children.0.props") || {};
-			if (!emojiId) return ret;
+			const { emojiId:id } = getNestedProp(ret, "props.children.0.props.children.0.props.children.0.props") || {};
+			if (!id) return ret;
 
 			const children = getNestedProp(ret, "props.children.0.props.children");
 
@@ -23,7 +23,7 @@ export default () => {
 					size={Button.Sizes.SMALL}
 					color={Button.Colors.GREEN}
 					onClick={() => {
-						const { url } = EmojiStore.getCustomEmojiById(emojiId) || {};
+						const  url = AssetURLUtils.getEmojiURL({id});
 						if (!url) return Toast.error("no url found");
 						copy(url);
 						Toast.success("Copid");
