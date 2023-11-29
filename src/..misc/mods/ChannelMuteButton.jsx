c3e53e6479300265bad499b13e4aafb679bf17f8
Skamt <114232893+Skamt@users.noreply.github.com>
2023-11-29 10:42:13 +0100

    10:42:11.96 - Wed 11/29/2023

--- src/..misc/mods/ChannelMuteButton.jsx
+++ src/..misc/mods/ChannelMuteButton.jsx
@@ -1,15 +1,14 @@
 import Logger from "@Utils/Logger";
 import { Disposable } from "@Utils";
 import { Patcher, React } from "@Api";
-import ChannelComponent from "@Patch/ChannelComponent";
+import ChannelComponent from "@Modules/ChannelComponent";
 import ChannelSettings from "@Modules/ChannelSettings";
 
 export default class ChannelMuteButton extends Disposable {
 	Init() {
-		const { module, key } = ChannelComponent;
-		if(module && key)
+		if (ChannelComponent)
 			this.patches = [
-				Patcher.after(module, key, (_, [{ muted, channel: { guild_id, id } }], ret) => {
+				Patcher.after(ChannelComponent, "default", (_, [{ muted, channel: { guild_id, id } }], ret) => {
 					const children = ret.props?.children?.props?.children[1].props?.children.props.children[1].props.children;
 					if (!children.some(a => a?.props?.id === "channelMuteButton"))
 						children?.push?.(
