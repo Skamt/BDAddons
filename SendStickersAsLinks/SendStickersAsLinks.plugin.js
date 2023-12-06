348d573ac361990ad15abf86f133bf9ff24f4194
Skamt <114232893+Skamt@users.noreply.github.com>
2023-12-06 12:57:34 +0100

    12:57:33.93 - Wed 12/06/2023

--- SendStickersAsLinks/SendStickersAsLinks.plugin.js
+++ SendStickersAsLinks/SendStickersAsLinks.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name SendStickersAsLinks
  * @description Enables you to send custom Stickers as links
- * @version 2.2.3
+ * @version 2.2.4
  * @author Skamt
  * @website https://github.com/Skamt/BDAddons/tree/main/SendStickersAsLinks
  * @source https://raw.githubusercontent.com/Skamt/BDAddons/main/SendStickersAsLinks/SendStickersAsLinks.plugin.js
@@ -10,7 +10,7 @@
 const config = {
 	"info": {
 		"name": "SendStickersAsLinks",
-		"version": "2.2.3",
+		"version": "2.2.4",
 		"description": "Enables you to send custom Stickers as links",
 		"source": "https://raw.githubusercontent.com/Skamt/BDAddons/main/SendStickersAsLinks/SendStickersAsLinks.plugin.js",
 		"github": "https://github.com/Skamt/BDAddons/tree/main/SendStickersAsLinks",
