02784803397252c21760023cdecc6472a24675ba
Skamt <114232893+Skamt@users.noreply.github.com>
2023-12-25 18:33:47 +0100

    18:33:46.00 - Mon 12/25/2023

--- src/Emojis/patches/patchFavoriteEmojis.js
+++ src/Emojis/patches/patchFavoriteEmojis.js
@@ -11,4 +11,19 @@ export default () => {
 			ret[0] = [...ret[0],...emojis];
 		});
 	else Logger.patch("emojiContextConstructor");
+
+	Patcher.after(emojiContextConstructor.prototype, "getDisambiguatedEmoji", (_, args, ret) => {
+		const emojis = Data.load("emojis");
+		let sum=[];
+		if(emojis.length > ret.length){
+			sum = [...emojis];
+			ret.forEach(r => emojis.find(e => e.id === r.id) ? null : sum.push(r));
+		}
+		else{
+			sum = [...ret];
+			emojis.forEach(r => ret.find(e => e.id === r.id) ? null : sum.push(r));
+		}
+		
+		return sum;
+	});
 };
