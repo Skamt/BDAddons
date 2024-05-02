420617443656d714b04be9353190a382549e3400
Skamt <114232893+Skamt@users.noreply.github.com>
2024-05-02 14:54:53 +0100

    14:54:52.21 - Thu 05/02/2024

--- src/..test/index.js
+++ src/..test/index.js
@@ -14,55 +14,6 @@ import { shallow } from "@Utils";
 import Logger from "@Utils/Logger";
 import Settings from "@Utils/Settings";
 import css from "./styles";
-import dummyMedia from "./test";
-
-const { closeModal, Popout, MenuItem, Menu, MenuGroup } = TheBigBoyBundle;
-
-const { FormTitle, Anchor } = TheBigBoyBundle;
-
-const album = {
-	...dummyMedia.album,
-	url:dummyMedia.album.external_urls.spotify
-};
-
-
-const banner = dummyMedia?.album?.images[1];
-
-function MoreInfo() {
-	// const { bannerMd: banner } = Store.state.getSongBanners();
-	
-
-	return (
-		<Flex style={{ padding: 10 }}>
-			<FormTitle tag="h4">Album: {album.id}</FormTitle>
-			<Flex
-				direction={Flex.Direction.HORIZONTAL}
-				style={{ gap: 10 }}>
-				<Flex grow={0}>
-					<Anchor href={album.url}>
-						<div
-							style={{
-								background: `url("${banner.url}") center/cover no-repeat`,
-								width: 150,
-								height: 150
-							}}></div>
-					</Anchor>
-				</Flex>
-				<Flex grow={1}>
-					<Flex direction={Flex.Direction.VERTICAL}>
-						<Anchor href={album.url}>{album.name}</Anchor>
-						<FormTitle tag="h5">Release date: {album.release_date}</FormTitle>
-						<FormTitle tag="h5">Total tracks: {album.total_tracks}</FormTitle>
-					</Flex>
-				</Flex>
-			</Flex>
-		</Flex>
-	);
-}
-
-
-// window._sdwed && closeModal(window._sdwed);
-// window._sdwed = showConfirmationModal(dummyMedia?.name, <MoreInfo />)
 
 export default () => {
 	return {
--- src/SpotifyEnhance/components/SpotifyPlayer.jsx
+++ src/SpotifyEnhance/components/SpotifyPlayer.jsx
@@ -1,15 +1,10 @@
-import { showConfirmationModal, React } from "@Api";
+import { React } from "@Api";
 import SpotifyPlayerControls from "./SpotifyPlayerControls";
 import TrackMediaDetails from "./TrackMediaDetails";
 import TrackTimeLine from "./TrackTimeLine";
-import Arrow from "@Components/Icons/Arrow";
 import { Store } from "../Store";
 import Settings from "@Utils/Settings";
 import { shallow } from "@Utils";
-import Tooltip from "@Components/Tooltip";
-import Button from "@Components/Button";
-import Flex from "@Components/Flex";
-import TheBigBoyBundle from "@Modules/TheBigBoyBundle";
 
 export default React.memo(function SpotifyPlayer() {
 	const [player, playerCompactMode, playerBannerBackground] = Settings(_ => [_.player, _.playerCompactMode, _.playerBannerBackground], shallow);
@@ -39,50 +34,6 @@ export default React.memo(function SpotifyPlayer() {
 
 			{mediaType === "track" && <TrackTimeLine />}
 			<SpotifyPlayerControls />
-			<Tooltip note="More info">
-				<Button
-					onClick={() => showConfirmationModal(media?.name, <MoreInfo />)}
-					size={Button.Sizes.NONE}
-					look={Button.Looks.BLANK}
-					innerClassName="flexCenterCenter"
-					className="spotify-player-more-info">
-					<Arrow />
-				</Button>
-			</Tooltip>
 		</div>
 	);
 });
-
-const { FormTitle, Anchor } = TheBigBoyBundle;
-
-function MoreInfo() {
-	const { bannerMd: banner } = Store.state.getSongBanners();
-	const album = Store.state.getAlbum();
-
-	return (
-		<Flex style={{ padding: 10 }}>
-			<FormTitle tag="h4">Album: {album.id}</FormTitle>
-			<Flex
-				direction={Flex.Direction.HORIZONTAL}
-				style={{ gap: 10 }}>
-				<Flex grow={0}>
-					<Anchor href={album.url}>
-						<div
-							style={{
-								background: `url("${banner.url}") center/cover no-repeat`,
-								width: 150,
-								height: 150
-							}}></div>
-					</Anchor>
-				</Flex>
-				<Flex grow={1}>
-					<Flex direction={Flex.Direction.VERTICAL}>
-						<Anchor href={album.url}>{album.name}</Anchor>
-						<FormTitle tag="h5">Release date: {album.release_date}</FormTitle>
-						<FormTitle tag="h5">Total tracks: {album.total_tracks}</FormTitle>
-					</Flex>
-				</Flex>
-			</Flex>
-		</Flex>
-	);
-}
