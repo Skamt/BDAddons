e4f9684b50e5ce0cc57710429e5a5917284b7e5d
Skamt <114232893+Skamt@users.noreply.github.com>
2024-05-16 11:25:56 +0100

    11:25:55.59 - Thu 05/16/2024

--- src/Emojis/patches/patchFavoriteEmojis.js
+++ src/Emojis/patches/patchFavoriteEmojis.js
@@ -5,25 +5,24 @@ import EmojiStore from "@Stores/EmojiStore";
 const emojiContextConstructor = EmojiStore?.getDisambiguatedEmojiContext?.().constructor;
 
 export default () => {
-	if (emojiContextConstructor)
-		Patcher.after(emojiContextConstructor.prototype, "rebuildFavoriteEmojisWithoutFetchingLatest", (_, args, ret) => {
-			const emojis = Data.load("emojis");
-			ret[0] = [...ret[0],...emojis];
-		});
-	else Logger.patch("emojiContextConstructor");
+	if (!emojiContextConstructor) return Logger.patch("emojiContextConstructor");
+
+	Patcher.after(emojiContextConstructor.prototype, "rebuildFavoriteEmojisWithoutFetchingLatest", (_, args, ret) => {
+		const emojis = Data.load("emojis");
+		ret[0] = [...ret[0], ...emojis];
+	});
 
 	Patcher.after(emojiContextConstructor.prototype, "getDisambiguatedEmoji", (_, args, ret) => {
 		const emojis = Data.load("emojis");
-		let sum=[];
-		if(emojis.length > ret.length){
+		let sum = [];
+		if (emojis.length > ret.length) {
 			sum = [...emojis];
-			ret.forEach(r => emojis.find(e => e.id === r.id) ? null : sum.push(r));
-		}
-		else{
+			ret.forEach(r => (emojis.find(e => e.id === r.id) ? null : sum.push(r)));
+		} else {
 			sum = [...ret];
-			emojis.forEach(r => ret.find(e => e.id === r.id) ? null : sum.push(r));
+			emojis.forEach(r => (ret.find(e => e.id === r.id) ? null : sum.push(r)));
 		}
-		
+
 		return sum;
 	});
 };
