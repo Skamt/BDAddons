5b6b8af9742786682365b52f1ccfa1d38e955346
Skamt <114232893+Skamt@users.noreply.github.com>
2024-07-03 14:28:43 +0100

    14:28:42.81 - Wed 07/03/2024

--- src/_misc/mods/EnableModView.js
+++ src/_misc/mods/EnableModView.js
@@ -1,20 +1,42 @@
 import Logger from "@Utils/Logger";
+import PermissionStore from "@Stores/PermissionStore";
 import { Disposable } from "@Utils";
+import { isSelf } from "@Utils/User";
 import { Patcher } from "@Api";
-// import { Filters, getModule } from "@Webpack";
+import { Filters,  getModule } from "@Webpack";
 
 // const a = getModule(a => a.useCanAccessGuildMemberModView);
+const d = getModule(Filters.byStrings("openGuildSidebar"), { searchExports: true });
 
 // eslint-disable-next-line no-undef
 
-
 export default class EnableModView extends Disposable {
 	Init() {
-		const a = s.getSource("checkElevated","autoTrackExposure")?.module?.exports;
+		this.patches.push(
+			Patcher.after(PermissionStore, "canAccessGuildSettings", () => true),
+			Patcher.after(PermissionStore, "can", (_, args, ret) => (args[0] === 268435456n ? true : ret))
+		);
+
+		const a = s.getSource("checkElevated", "autoTrackExposure")?.module?.exports;
 		if (!a) return Logger.patch("EnableModView");
 
-		for(const key in a){
-			this.patches.push(Patcher.after(a, key,() => true));
+		for (const key in a) {
+			this.patches.push(Patcher.after(a, key, () => true));
 		}
+
+		this.patches.push(
+			BdApi.ContextMenu.patch("user-context", (retVal, args) => {
+				const { user, channel, guildId } = args;
+				if (!isSelf(user)) return;
+				retVal.props.children.splice(
+					1,
+					0,
+					BdApi.ContextMenu.buildItem({
+						label: "Mod View",
+						action: () => d(guildId, user.id, channel.id)
+					})
+				);
+			})
+		);
 	}
 }
