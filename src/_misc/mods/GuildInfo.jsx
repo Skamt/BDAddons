933a3bfbc8a004de0b73dab922e852af89f7cade
Skamt <114232893+Skamt@users.noreply.github.com>
2024-08-19 18:04:26 +0100

    18:04:25.30 - Mon 08/19/2024

--- src/_misc/mods/GuildInfo.jsx
+++ src/_misc/mods/GuildInfo.jsx
@@ -1,6 +1,6 @@
 import Logger from "@Utils/Logger";
-import { Disposable, parseSnowflake } from "@Utils";
-import { Patcher, React } from "@Api";
+import { getImageModalComponent, openModal, Disposable, parseSnowflake } from "@Utils";
+import { ContextMenu, Patcher, React } from "@Api";
 
 import GuildTooltip from "@Patch/GuildTooltip";
 
@@ -26,20 +26,22 @@ export default class GuildInfo extends Disposable {
 			this.patches = [
 				Patcher.after(module, key, (_, [{ guild }], ret) => {
 					// console.log(guild);
-					const owner = UserStore.getUser(guild.ownerId)
-					ret.props.text = [
-						ret.props.text,
-						el(`Owner: ${owner?.globalName || owner?.username || guild.ownerId}`),
-						el(`Created At: ${new Date(parseSnowflake(+guild.id)).toLocaleDateString()}`),
-						el(`Joined At: ${guild.joinedAt.toLocaleDateString()}`),
-						el("Clyde", { style: { color: guild.features.has(GuildFeaturesEnum.CLYDE_ENABLED) ? "lime" : "red" } }),
-						el(`Roles: ${Object.keys(GuildStore.getRoles(guild.id)).length}`),
-						el(`Channels: ${GuildChannelStore.getChannels(guild.id).count}`),
-						el(`Members: ${GuildMemberCountStore.getMemberCount(guild.id)}`),
-					]
+					const owner = UserStore.getUser(guild.ownerId);
+					ret.props.text = [ret.props.text, el(`Owner: ${owner?.globalName || owner?.username || guild.ownerId}`), el(`Created At: ${new Date(parseSnowflake(+guild.id)).toLocaleDateString()}`), el(`Joined At: ${guild.joinedAt.toLocaleDateString()}`), el("Clyde", { style: { color: guild.features.has(GuildFeaturesEnum.CLYDE_ENABLED) ? "lime" : "red" } }), el(`Roles: ${Object.keys(GuildStore.getRoles(guild.id)).length}`), el(`Channels: ${GuildChannelStore.getChannels(guild.id).count}`), el(`Members: ${GuildMemberCountStore.getMemberCount(guild.id)}`)];
+				}),
+				ContextMenu.patch("guild-context", (retVal, { guild }) => {
+					if (!guild) return;
+					const banner = guild.getIconURL(4096);
+					if (!banner) return;
+					retVal.props.children.splice(1,0,
+						ContextMenu.buildItem({
+							label: "View logo",
+							action: () => openModal(<div>{getImageModalComponent(banner, { width:4096, height:4096 })}</div>)
+							
+						})
+					);
 				})
 			];
-		else
-			Logger.patch("GuildInfo");
+		else Logger.patch("GuildInfo");
 	}
 }
