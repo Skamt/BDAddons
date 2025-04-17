f106927cae5bbdf7b6dda6e2cce80c40ceba0f16
Skamt <114232893+Skamt@users.noreply.github.com>
2025-04-17 14:31:07 +0000

    14:31:07.35 - Thu 04/17/2025

--- StickerEmojiPreview/StickerEmojiPreview.plugin.js
+++ StickerEmojiPreview/StickerEmojiPreview.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name StickerEmojiPreview
  * @description Adds a zoomed preview to those tiny Stickers and Emojis
- * @version 1.2.5
+ * @version 1.2.6
  * @author Skamt
  * @website https://github.com/Skamt/BDAddons/tree/main/StickerEmojiPreview
  * @source https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/StickerEmojiPreview.plugin.js
@@ -10,7 +10,7 @@
 const config = {
 	"info": {
 		"name": "StickerEmojiPreview",
-		"version": "1.2.5",
+		"version": "1.2.6",
 		"description": "Adds a zoomed preview to those tiny Stickers and Emojis",
 		"source": "https://raw.githubusercontent.com/Skamt/BDAddons/main/StickerEmojiPreview/StickerEmojiPreview.plugin.js",
 		"github": "https://github.com/Skamt/BDAddons/tree/main/StickerEmojiPreview",
@@ -111,7 +111,7 @@ Object.defineProperty(SettingsStore, "state", {
 const Settings = SettingsStore;
 
 // @Modules\Popout
-const Popout = getModule(Filters.byKeys("Animation", "defaultProps"), { searchExports: true });
+const Popout = getModule(Filters.byPrototypeKeys("shouldShowPopout", "toggleShow"), { searchExports: true });
 
 // src\StickerEmojiPreview\Constants.js
 const PREVIEW_SIZE = 300;
