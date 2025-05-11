8ceb69e9bdf54c20adb32c47dec74a600d1b7a97
Skamt <114232893+Skamt@users.noreply.github.com>
2025-05-11 22:48:06 +0000

    22:48:05.70 - Sun 05/11/2025

--- src/Tabbys/components/Tab/GenericTab.jsx
+++
@@ -1,7 +0,0 @@
-import React from "@React";
-// import { Store } from "@/Store";
-import BaseTab from "./BaseTab";
-
-export default function GenericTab(props) {
-	return <BaseTab {...props} />;
-}
--- src/Tabbys/components/Tab/index.jsx
+++ src/Tabbys/components/Tab/index.jsx
@@ -2,26 +2,25 @@ import "./styles";
 import React from "@React";
 import { Store } from "@/Store";
 import { shallow } from "@Utils";
-import GenericTab from "./GenericTab";
+import BaseTab from "./BaseTab";
 import ChannelTab from "./ChannelTab";
 
 export default React.memo(function TabSwitch({ tabId }) {
 	const item = Store(state => state.getTab(tabId), shallow);
 	if (!item?.path) return;
-	const [, type, guildId, channelId, , threadId] = item.path.split("/");
+	const [, type, , channelId, , threadId] = item.path.split("/");
 
 	if (type === "channels" && channelId)
 		return (
 			<ChannelTab
 				tabId={tabId}
 				path={item.path}
-				guildId={guildId}
 				channelId={threadId || channelId}
 			/>
 		);
 
 	return (
-		<GenericTab
+		<BaseTab
 			tabId={tabId}
 			path={item.path}
 			title={type}
