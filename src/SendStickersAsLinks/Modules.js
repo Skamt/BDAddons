8342e8fe354656ef7c06b091d7a02c639b679050
Skamt <114232893+Skamt@users.noreply.github.com>
2025-07-15 22:02:58 +0000

    22:02:57.85 - Tue 07/15/2025

--- src/SendStickersAsLinks/Modules.js
+++ src/SendStickersAsLinks/Modules.js
@@ -1,13 +1,11 @@
 import {  getMangled, Filters } from "@Webpack";
 
 export const StickerSendability = getMangled(
-	/SENDABLE_WITH_BOOSTED_GUILD.{1,3}/,
+	a => typeof a === "object" && "SENDABLE" in a,
 	{
 		StickersSendabilityEnum: Filters.byKeys("SENDABLE_WITH_PREMIUM"),
 		getStickerSendability: Filters.byStrings("canUseCustomStickersEverywhere"),
 		isSendableSticker: Filters.byStrings("0===")
 	},
-	// { searchExports: true, raw:true }
+	{ searchExports: true, raw:true }
 );
-
-console.log(StickerSendability)
